8487678280ca05e156def800d58ab4e8a0610389
Luiz Henrique <7henrique18@gmail.com>
2023-04-16 14:56:40 -0300

    chore: Change README

--- src/use-cases/factories/make-org-register-use-case.ts
+++ src/use-cases/factories/make-org-register-use-case.ts
@@ -1,9 +1,9 @@
 import { PrismaOrgsRepository } from '@/repositories/prisma/prisma-orgs-repository'
-import { RegisterUseCase } from '../register-org'
+import { RegisterOrgUseCase } from '../register-org'
 
 export function makeOrgRegisterUseCase() {
   const orgsRepository = new PrismaOrgsRepository()
-  const registerUseCase = new RegisterUseCase(orgsRepository)
+  const registerUseCase = new RegisterOrgUseCase(orgsRepository)
 
   return registerUseCase
 }
